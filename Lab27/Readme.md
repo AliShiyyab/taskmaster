488ae2b78706839d1fd37faa0fca4a878e3e7435
AliShiyyab <alishiyyab1998@gmail.com>
2021-08-30 18:26:35 +0300

    Lab27

---
+++ Lab27/Readme.md
@@ -0,0 +1,22 @@
+# Lab27
+
+Today :
+
+* Create new 4 Button.
+* Create Tow Activity Empty Page. 
+* send a text from page to page.
+* get element of item from page to page.
+
+## ScreenShots:
+
+>### Replace my name with loren Iposem
+
+![](201.jpeg)
+
+![](301.jpeg)
+
+![](401.jpeg)
+
+![](Home.jpeg)
+
+![](AfterEdit.jpeg)
---
+++ app/src/main/java/com/example/taskmaster/Setting.java
@@ -0,0 +1,34 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        Button btn = findViewById(R.id.button5);
+
+        btn.setOnClickListener((View) -> {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Setting.this);
+            SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+
+            EditText userNameField = findViewById(R.id.editTextTextPersonName);
+            String userName = userNameField.getText().toString();
+
+            sharedPreferencesEditor.putString("userName", userName);
+            sharedPreferencesEditor.apply();
+        });
+    }
+}
