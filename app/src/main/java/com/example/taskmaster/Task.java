37a3c78f639de7f24588b6013e371477b6febd67
AliShiyyab <alishiyyab1998@gmail.com>
2021-09-01 01:26:43 +0300

    Lab28

---
+++ app/src/main/java/com/example/taskmaster/Task.java
@@ -0,0 +1,14 @@
+package com.example.taskmaster.tasks;
+
+public class Task {
+    // variable:
+    public String title;
+    public String body;
+    public String state;
+
+    public Task(String title, String body, String state) {
+        this.title = title;
+        this.body = body;
+        this.state = state;
+    }
+}
